22eca5923d35e51a5fc937ff6cbb0af668dd7248
Gustavo Moda <gustavo.moda@gmail.com>
2022-10-31 20:45:28 -0300

    POC Clean Architecture (#1)
    * Ignore files
    
    * Add flutter packages
    
    * Add Login feature files and the folder structure
    
    * Add User model
    
    * Add get it

---
+++ lib/src/injector.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'injector.config.dart';
+
+final injector = GetIt.instance;
+
+@module
+abstract class RegisterModule {}
+
+@InjectableInit(
+  initializerName: r'$initGetIt', // default
+  preferRelativeImports: true, // default
+  asExtension: false, // default
+)
+void configureDependencies([String environment = Environment.prod]) =>
+    $initGetIt(
+      injector,
+      environment: environment,
+    );
---
+++ test/fixtures/fakers/pojo_faker.dart
@@ -0,0 +1,27 @@
+import 'package:faker/faker.dart';
+
+abstract class PojoFaker<T> {
+  const PojoFaker(this.faker);
+
+  final Faker faker;
+
+  // Generate a single fake model
+  T generate();
+
+  // Generate a list of fake models
+  List<T> generateList({int? length}) {
+    return List.generate(
+      length ?? faker.randomGenerator.integer(100, min: 0),
+      (index) => generate(),
+    );
+  }
+
+  // Generate a random amount greater than or equal to one with 2 decimal places.
+  double get randomAmount => faker.randomGenerator.decimal(scale: 2, min: 1);
+
+  // Generate a random id
+  int get randomId => faker.randomGenerator.integer(
+        double.infinity.toInt(),
+        min: 1,
+      );
+}
